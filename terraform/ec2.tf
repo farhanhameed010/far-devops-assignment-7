65cd1e35dd140ac003e8890bebb84067fe87859e
farhanhameed010 <farhanhameed010@gmail.com>
2025-01-16 12:10:15 +0500

    test

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "ec2_sg" {
 # Create Key Pair
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
-  public_key = var.public_key
+  public_key = var.ssh_public_key
 
   tags = {
     Name        = "${var.environment}-keypair"
